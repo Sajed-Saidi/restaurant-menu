11476e0f905a067e38962a537c95f61794abfc34
Sajed Saidi <programmingstudent8@gmail.com>
2024-12-15 16:45:24 +0200

    try settings add

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -51,9 +51,9 @@ class AdminPanelProvider extends PanelProvider
                 SubstituteBindings::class,
                 DisableBladeIconComponents::class,
                 DispatchServingFilamentEvent::class,
+            ])
+            ->authMiddleware([
+                Authenticate::class,
             ]);
-        // ->authMiddleware([
-        //     Authenticate::class,
-        // ]);
     }
 }
