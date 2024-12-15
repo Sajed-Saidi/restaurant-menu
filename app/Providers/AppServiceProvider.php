4d701a1c0dfbc7dd9db2c051ebeb7cbc08ecb4fa
Sajed Saidi <programmingstudent8@gmail.com>
2024-12-15 16:00:36 +0200

    adsf

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,27 +23,27 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        Schema::defaultStringLength(191);
-        Blade::withoutDoubleEncoding();
+        // Schema::defaultStringLength(191);
+        // Blade::withoutDoubleEncoding();
 
-        if (Setting::count() > 0) {
-            $settings = Setting::first();
-        } else {
-            $settings =  [
-                "id" => "1",
-                "website_name" => "Website",
-                "email" => "website@gmail.com",
-                "phone" => "+961 71 595 345",
-                "address" => "Tyre, Lebanon",
-                "meta_description" => "test",
-                "meta_keywords" => "hello there",
-                "logo" => "logo.png",
-                "facebook_url" => "https://facebook.com/",
-                "instagram_url" => "https://instagram.com/",
-                "created_at" => "2024-12-05 19:34:08",
-                "updated_at" => "2024-12-05 20:37:43"
-            ];
-        }
-        View::share('settings', (object) $settings);
+        // if (Setting::count() > 0) {
+        //     $settings = Setting::first();
+        // } else {
+        //     $settings =  [
+        //         "id" => "1",
+        //         "website_name" => "Website",
+        //         "email" => "website@gmail.com",
+        //         "phone" => "+961 71 595 345",
+        //         "address" => "Tyre, Lebanon",
+        //         "meta_description" => "test",
+        //         "meta_keywords" => "hello there",
+        //         "logo" => "logo.png",
+        //         "facebook_url" => "https://facebook.com/",
+        //         "instagram_url" => "https://instagram.com/",
+        //         "created_at" => "2024-12-05 19:34:08",
+        //         "updated_at" => "2024-12-05 20:37:43"
+        //     ];
+        // }
+        // View::share('settings', (object) $settings);
     }
 }
